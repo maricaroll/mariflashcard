1b82e44c2a2ac568e62f58d095205932daf10660
maricaroll <00001099842931SP@al.educacao.sp.gov.br>
2024-11-14 14:05:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filme',
+    'Qual é o verdadeiro nome do Superman?',
+    'O verdadeiro nome do Superman é Clark Kent'
+)
+
+criaCartao(
+    'História',
+    'Por quanto tempo a escravidão durou no Brasil?',
+    'A escravidão no Brasil durou por 388 anos'
+)
+
+criaCartao(
+    'Série',
+    'Os Simpsons tem quantas temporadas?',
+    'Os Simpsons tem 35 temporadas'
+)
+
+criaCartao(
+    'Filme',
+    'Quem é o pai do Luke Skywalker?',
+    'O pai do Luke Skywalker é o Darth Vader'
+)
